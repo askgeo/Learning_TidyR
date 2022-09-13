b269b799747665d990a06027c88f5d539f4fe68b
Abigail Knapp <abigail.s.knapp@gmail.com>
2022-09-13 19:07:40 -0400

    Beginning my workspace, creating main R script, following along at the https://r4ds.had.co.nz/tidy-data.html site

---
+++ Learning_TidyR.R
@@ -0,0 +1,20 @@
+# Learning Tidyverse
+# A. Knapp Fall 2022
+
+# following along at https://r4ds.had.co.nz/tidy-data.html
+
+#------------------------------
+#         LIBRARIES
+#------------------------------
+
+library(tidyverse) # for tidyr and more essentials
+
+#------------------------------
+#         MAKING TIDY TABLES
+#------------------------------
+
+
+
+#------------------------------
+#         GGPLOTTING
+#------------------------------
